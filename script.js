05247b2b70b0f8c98faa02ef7902af312e04dd25
THACHER <sophie.thacher@gmail.com>
2016-02-22 16:26:01 -0800

    first checkerboard works!

--- script.js
+++ script.js
@@ -2,14 +2,17 @@
 var body = document.getElementsByTagName('body')[0];
 
 for(var i = 0; i < 9; i++){
-  for(var j = 0; i < 9; j++){
+  for(var j = 0; j < 9; j++){
     var squares = document.createElement('div');
     squares.style.height = '11.1%';
     squares.style.width = '11.1%';
     squares.style.float = 'left';
     squares.style.paddingTop = '10%';
-    squares.style.border = '2px black solid'
-    squares.style.backgroundColor = 'green';
+      if((j + i) % 2 === 0){
+        squares.style.backgroundColor = 'black';
+      } else {
+        squares.style.backgroundColor = 'red';
+      }
     body.appendChild(squares);
   }
 }
