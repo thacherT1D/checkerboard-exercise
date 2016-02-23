d30d025b6740afab547eca4df165bc438e83fa71
THACHER <sophie.thacher@gmail.com>
2016-02-22 21:03:16 -0800

    gradient edition

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 // Your JS goes here
 var body = document.getElementsByTagName('body')[0];
+// body.style.background = linearGradient(135, red, blue);
 
 for(var i = 0; i < 9; i++){
   for(var j = 0; j < 9; j++){
+    var gradientValue = (i / 10);
     var squares = document.createElement('div');
     squares.style.height = '11.1%';
     squares.style.width = '11.1%';
     squares.style.float = 'left';
     squares.style.paddingTop = '10%';
       if((j + i) % 2 === 0){
-        squares.style.backgroundColor = 'black';
+        squares.style.backgroundColor = `rgba(0, 0, 255, ${gradientValue})`;
       } else {
-        squares.style.backgroundColor = 'red';
+        squares.style.backgroundColor = 'rgba(0, 255, 0, ' + gradientValue  + ')';
       }
     body.appendChild(squares);
   }
