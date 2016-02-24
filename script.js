fa5021da06b02a71593b07f0ddcdc52c163e8627
THACHER <sophie.thacher@gmail.com>
2016-02-24 11:53:49 -0800

    updates from Linter

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 // Your JS goes here
-var body = document.getElementsByTagName('body')[0];
+(function makeBoard() {
+  'use strict';
+  var body = document.getElementsByTagName('body')[0];
 
-for(var i = 0; i < 9; i++){
-  for(var j = 0; j < 9; j++){
-    var squares = document.createElement('div');
-    squares.style.height = '11.1%';
-    squares.style.width = '11.1%';
-    squares.style.float = 'left';
-    squares.style.paddingTop = '10%';
-      if((j + i) % 2 === 0){
+  for (var i = 0; i < 9; i++) {
+    for (var j = 0; j < 9; j++) {
+      var squares = document.createElement('div');
+
+      squares.style.height = '11.1%';
+      squares.style.width = '11.1%';
+      squares.style.float = 'left';
+      squares.style.paddingTop = '10%';
+      if ((j + i) % 2 === 0) {
         squares.style.backgroundColor = 'black';
-      } else {
+      }
+      else {
         squares.style.backgroundColor = 'red';
       }
-    body.appendChild(squares);
+      body.appendChild(squares);
+    }
   }
-}
+})();
